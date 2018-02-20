9b75fc15aa1b3613dbc0791dbce1674db3866d64
Siarhei <solimr111@mail.ru>
2018-02-20 18:47:04 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 class Sorter {
-  constructor() {
+    constructor() {
     this.newSorter = [];
-    this.comparator = (left,right) => right - left;
+    this.comparator = (left,right) => left-right;
   }
 
   add(element) {
@@ -17,24 +17,23 @@ class Sorter {
   }
 
   toArray() {
-    let currentToArray = this.newSorter.slice();
-    return currentToArray;
+    return this.newSorter;
   }
 
   sort(indices) {
-    let bufferArray = [], finalArray = [];
+    let bufferArray = [];
 
-    for (let index = 0; index < indices.length; index++) {
-      bufferArray[index] = indices[index];
-    }
+    bufferArray = indices.sort().map((index) => this.newSorter[index]);
     bufferArray.sort(this.comparator);
 
-    return bufferArray;
+    for(let index = 0; index < indices.length; index++){
+      this.newSorter[indices[index]] = bufferArray[index];
+      }
+    return this.newSorter;
   }
 
   setComparator(compareFunction) {
     this.comparator = compareFunction;
   }
 }
-
 module.exports = Sorter;
